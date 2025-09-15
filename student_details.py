c3d2e6bd2350ebf4dfa1fb0c134977d586ddede6
Rani Rathore <2020btechairani8719@poornima.edu.in>
2025-09-15 13:06:03 +0530

    Create student_details.py

---
+++ student_details.py
@@ -0,0 +1,5 @@
+# Write a program to create a class with the name Student and perform the following tasks - 1. Declare a variable grade 2. Print a sentence inside the class 3. Create an object of class student and see the output
+class Student:
+    grade=6
+    print("I am one of the inteligent students of this grade ",grade)
+Mahd=Student()
